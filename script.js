c70d050bb82bb758ab24545fc6d3ea147dbeeaf4
gbeemiga <damola454@gmail.com>
2022-06-09 16:38:04 +0100

    Detailed attendance register

---
+++ script.js
@@ -0,0 +1,81 @@
+function myMark1 (element) {
+    var x = parseInt(document.getElementById(`txt1`).value)
+    if (element === 'present1') {
+        document.getElementById(`txt1`).value = x + 1
+    } else if (element === `late1`){
+        document.getElementById(`txt1`).value = x + 1
+    }
+}
+
+function myMark2 (element) {
+
+    var x = parseInt(document.getElementById(`txt2`).value)
+    if (element === 'present2') {
+        document.getElementById(`txt2`).value = x + 1
+    } else if (element === `late2`){
+        document.getElementById(`txt2`).value = x + 1
+    }
+}
+
+function myMark3 (element) {
+
+    var x = parseInt(document.getElementById(`txt3`).value)
+    if (element === 'present3') {
+        document.getElementById(`txt3`).value = x + 1
+    } else if (element === `late3`){
+        document.getElementById(`txt3`).value = x + 1
+    }
+}
+
+function myMark4 (element) {
+
+    var x = parseInt(document.getElementById(`txt4`).value)
+    if (element === 'present4') {
+        document.getElementById(`txt4`).value = x + 1
+    } else if (element === `late4`){
+        document.getElementById(`txt4`).value = x + 1
+    }
+}
+
+function myMark5 (element) {
+
+    var x = parseInt(document.getElementById(`txt5`).value)
+    if (element === 'present5') {
+        document.getElementById(`txt5`).value = x + 1
+    } else if (element === `late5`){
+        document.getElementById(`txt5`).value = x + 1
+    }
+}
+
+function myMark6(element) {
+    var x = parseInt(document.getElementById(`txt6`).value)
+    if (element === `present6`) {
+        document.getElementById(`txt6`).value = x + 1
+    } else if ( element === `late6`) {
+        document.getElementById(`txt6`).value = x + 1
+    }
+} 
+
+function myMark7 (element) {
+
+    var x = parseInt(document.getElementById(`txt7`).value)
+    if (element === 'present7') {
+        document.getElementById(`txt7`).value = x + 1
+    } else if (element === `late7`){
+        document.getElementById(`txt7`).value = x + 1
+    }
+}
+
+function myMark8(element) {
+    var x = parseInt(document.getElementById(`txt8`).value)
+    if (element === `present8` || element === `late8`) {
+        document.getElementById(`txt8`).value = x + 1
+    }
+}
+
+function myMark9(element) {
+    var x = parseInt(document.getElementById(`txt9`).value)
+    if (element === `present9` || element === `late9`) {
+        document.getElementById(`txt9`).value = x + 1
+    }
+}
